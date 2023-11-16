710288ca5c9bd5e7047a83778dd10e34c5ee2d88
PoomNattapoom <110966644+PoomNattapoom@users.noreply.github.com>
2023-11-16 19:32:24 +0700

    submit lab01 (#2)
    * finished 01
    
    * finished 02

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,28 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+import java.util.*;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.print("Hello and welcome!");
-
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        ArrayList<Integer> result = sortTheArray(n);
+        for(int i=0 ;i<n;i++){
+            System.out.println(result.get(i)+" ");
         }
+
+    }
+    public static ArrayList<Integer> sortTheArray(int n){
+        ArrayList<Integer> arr = new ArrayList<>();
+        for(int i=0 ; i<n ; i++){
+            Scanner s1 = new Scanner(System.in);
+            int x = s1.nextInt();
+            arr.add(x);
+        }
+        Collections.sort(arr);
+        return arr;
     }
 }
